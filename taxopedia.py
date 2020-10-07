ccea0e163591a1707375f1c28f2c0cf9d9bf7e28
Shane Drabing <shane@Shanes-MacBook-Pro.local>
2020-10-07 13:13:22 -0700

    cleaned up work space

--- taxopedia.py
+++ taxopedia.py
@@ -12,6 +12,24 @@ from async_utils import run_requests, divide_chunks
 
 PATTERN = re.compile(r"(/wiki/\w+:)")
 
+RANK = [
+    "Rank", "Common Name", "Domain", "Subdomain", "Realm", "Subrealm",
+    "Hyperkingdom", "Superkingdom", "Kingdom", "Subkingdom", "Infrakingdom",
+    "Parvkingdom", "Superphylum", "Phylum", "Subphylum", "Infraphylum",
+    "Microphylum", "Superclass", "Class", "Subclass", "Infraclass",
+    "Parvclass", "Superdivision", "Division", "Subdivision", "Infradivision",
+    "Superlegion", "Legion", "Sublegion", "Infralegion", "Supercohort",
+    "Cohort", "Subcohort", "Infracohort", "Gigaorder", "Magnorder",
+    "Grandorder", "Mirorder", "Superorder", "Series", "Order", "Parvorder",
+    "Nanorder", "Hypoorder", "Minorder", "Suborder", "Infraorder",
+    "Parvorder", "Section", "Subsection", "Gigafamily", "Megafamily",
+    "Grandfamily", "Hyperfamily", "Superfamily", "Epifamily", "Series",
+    "Group", "Family", "Subfamily", "Infrafamily", "Supertribe", "Tribe",
+    "Subtribe", "Infratribe", "Genus", "Subgenus", "Section", "Subsection",
+    "Series", "Subseries", "Superspecies", "Species", "Subspecies",
+    "Variety", "Subvariety", "Form", "Subform"
+]
+
 
 # FUNCTIONS
 
